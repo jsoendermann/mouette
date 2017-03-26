9e516e71221bad9eebd69b7c7ae9f74081270f33
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-03-27 00:32:06 +0800

    :white_check_mark: Add second test that uses TestDbWrapper and jest mocks

--- __tests__/rules/collectionNamesNumber.test.ts
+++ __tests__/rules/collectionNamesNumber.test.ts
@@ -1,7 +1,9 @@
 import { Rule } from '../../src/rules/collectionNamesNumber'
+jest.mock('../../src/db')
+import { TestDbWrapper } from '../../src/db'
 
 describe('collection-names-number', () => {
-  it('has correct metadata', async () => {
+  it('should have correct metadata', async () => {
     const rule = new Rule({
       number: 'plural',
     })
@@ -9,5 +11,21 @@ describe('collection-names-number', () => {
     const m = rule.getMetadata()
 
     expect(m).toMatchSnapshot()
+  })
+
+  it("should work when number is 'singular'", async () => {
+    const db = new TestDbWrapper();
+
+    (db.getCollectionNames as jest.Mock<any>).mockReturnValueOnce(['cow', 'cats'])
+
+    const rule = new Rule({
+      number: 'singular',
+    })
+
+    const result = await rule.apply(db)
+
+    console.log(result)
+
+    expect(result).toMatchSnapshot()
   })
 })
