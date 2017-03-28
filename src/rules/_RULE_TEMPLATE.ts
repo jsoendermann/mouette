258b4b59df3c5a7628d6ef0f6875086bc6ab5f96
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-03-28 17:26:53 +0800

    :bulb: Update _RULE_TEMPLATE.ts

--- src/rules/_RULE_TEMPLATE.ts
+++ src/rules/_RULE_TEMPLATE.ts
@@ -2,16 +2,18 @@ import * as Joi from 'joi'
 
 import { IDb } from '../db'
 import {
-  AbstractRule,
+  AbstractCollectionRule,
   IRuleFailureSpecificJson,
   RuleFailure,
   RuleGranularity,
-  RuleSeverity,
 } from '../rule'
 
 
-// Don't change the name of this class
-export class Rule extends AbstractRule {
+// You should either inherit from AbstractCollectionRule or AbstractKeyRule,
+// depending on whether your rule needs just the collection name or both
+// the collection name and the name of a key on the collection to run.
+// Also, please don't change the name from "Rule" to anything else.
+export class Rule extends AbstractCollectionRule {
   public static metadata = {
     // The name of your rule in kebab case.
     name: 'my-new-rule',
@@ -21,11 +23,13 @@ export class Rule extends AbstractRule {
     description: 'Makes sure collections collect.',
     // A justification for why it exists.
     rationale: 'Because we say so.',
-    // See rule.ts for possible values. What your rule operates on.
-    severity: RuleSeverity.Warning,
-    // What your rule operates on.
+    // What your rule operates on. Usually, this will correspond to the
+    // superclass you are inheriting from but if you are using map reduce
+    // to loop over keys, you might want to inherit from AbstractCollectionRule
+    // and still set this to RuleGranularity.KeyName or RuleGranularity.Column.
     granularity: RuleGranularity.CollectionName,
-    // Whether any guessing is involved in your rule.
+    // Whether any guessing is involved in your rule. This applies
+    // only to the rule itself, not to the suggestions it makes.
     isFuzzy: false,
     // A string that contains a toml-encoded object with the options
     // to your rule. Possible types are: boolean, string, number,
@@ -54,17 +58,23 @@ export class Rule extends AbstractRule {
 
   public getMetadata() { return Rule.metadata }
 
-  // This method takes an IDb object and returns an array with all the
-  // rule violations in the database. You can access your options as
-  // this.options['my-first-option']. If you need to execute any db
-  // operations that are not yet part of the IDb interface, you need to
-  // add it in db.ts. RuleFailures are constructed like so:
-  // new RuleFailure(
-  //   this,
-  //   collectionName, // If your rule operates on collections
-  //   keyName, // If it also operates on keys or columns
-  // )
-  public async getFailures(db: IDb): Promise<RuleFailure[]> {
+  // This is the body of your rule class that looks for violations.
+  // You can access your options as this.options['my-first-option'].
+  // If you need to execute any db operations that are not yet part of
+  // the IDb interface, you need to add them in db.ts.
+  public async getFailuresForCollection(
+    db: IDb,
+    collectionName: string,
+  ): Promise<RuleFailure[]> {
+    // RuleFailures are constructed like so:
+    // new RuleFailure(
+    //   this,
+    //   {
+    //     collectionName, // If your rule operates on collections
+    //     keyName, // If it also operates on keys or columns
+    //     additionalDetails, // Anything you need in getFailureSpecificJson
+    //   },
+    // )
     return []
   }
 
@@ -72,7 +82,9 @@ export class Rule extends AbstractRule {
   // - a 'failure' property that describes the rule violation that occurred
   //   e.g. 'Collection name cow is not pluralized.'
   // - (optionally) a 'suggestion' property that suggests a fix to the problem
-  //   e.g. 'Change cow to cows.'
+  //   e.g. 'Change cow to cows.' This suggestion should add value beyond just
+  //   saying "Don't break this rule". If no meaningful suggestion can be made,
+  //   it's better to leave this undefined.
   // - (optionally) a 'mongoCommand' property that contains a string ready to
   //   be executed on the user's MongoDB that shows him all the records that
   //   violate this rule. MAKE SURE RUNNING THIS COMMAND DOES NOT ALTER The
